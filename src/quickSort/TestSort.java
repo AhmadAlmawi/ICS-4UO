93b319c86e8e735780083c4011aa77785bf38221
Ahmad Almawi <ahmadalmawi@icloud.com>
2018-06-26 22:56:45 -0400

    Added stopwatch to time sorts

--- src/quickSort/TestSort.java
+++ src/quickSort/TestSort.java
@@ -1,5 +1,7 @@
 package quickSort;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,17 +17,19 @@ public class TestSort {
 		int numItems = input.nextInt();
 		int[] array = new int[numItems];	
 		for (int i = 0; i < numItems; i++) {
-			int ranNum = ThreadLocalRandom.current().nextInt(0, 1000);
+			int ranNum = ThreadLocalRandom.current().nextInt(0, 100000);
 			array[i] = ranNum; //fills the array with the number of randomly generated values the user requested
 		}
 		int lower = 0;
 		int upper = array.length -1;
 		System.out.println("Your unsorted array is: ");		
 		System.out.println(Arrays.toString(array));
+		Instant starts = Instant.now();
 		testSort.quickSort(array, lower, upper);
+		Instant ends = Instant.now();
 		System.out.println("Your sorted array is: ");
 		System.out.println(Arrays.toString(array));
-	
+		System.out.println(Duration.between(starts, ends));
 
 	}
 
